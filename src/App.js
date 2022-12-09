4555125cbe3610b8b2a1d4fd88185d2167d2db1c
MUHAMMAD IQBAL <43297852+mhdiqbal19@users.noreply.github.com>
2022-12-10 01:12:14 +0700

    object didalam state class componen

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Iqbal'
+      name: {firstName: 'Muhammad', lastName: 'Iqbal'},
+      company: 'ZTM'
     }
 
   }
@@ -18,10 +19,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name}
+            Hi {this.state.name.firstName} {this.state.name.lastName}, Iam work in {this.state.company}
           </p>
           <button onClick={() => {
-            this.setState({name: 'Andrei'});
+            this.setState({name: {firstName: 'Andrei', lastName: 'Taulany'}});
+            console.log(this.state);
           }}>Change Name</button>
         </header>
       </div>
