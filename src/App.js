0995d9d5c211c6313255d2c7791cb4252c565d4e
MUHAMMAD IQBAL <43297852+mhdiqbal19@users.noreply.github.com>
2022-12-20 22:45:42 +0700

    map and key"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
@@ -7,31 +6,36 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {firstName: 'Muhammad', lastName: 'Iqbal'},
-      company: 'ZTM'
-    }
+      monsters: [
+        {
+          name: 'iqbal',
+          id: '12232',
+        },
+        {
+          name: 'ridwan',
+          id: '23421'
+        },
+        {
+          name: 'jira',
+          id: '53443'
+        },
+      ]
 
-  }
- 
+    };
+  };
+
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, Iam work in {this.state.company}
-          </p>
-          <button onClick={() => {
-            this.setState(
-              (state, props) => {
-                return {
-                  name: {firstName: 'Muhammad', lastName: 'Ahsan'},
-                };
-              },
-          );
-        }}
-          >Change Name</button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return( 
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+            )
+          })
+        }
       </div>
     );
   }
