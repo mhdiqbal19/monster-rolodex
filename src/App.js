a9f46306f02168f1c4b4618597b213243ab224b7
MUHAMMAD IQBAL <43297852+mhdiqbal19@users.noreply.github.com>
2022-12-23 16:09:13 +0700

    optimizations

--- src/App.js
+++ src/App.js
@@ -26,11 +26,20 @@ class App extends Component {
       );
   }
 
-  render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
     });
-    console.log("filter", filteredMonsters);
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
 
     return (
       <div className="App">
@@ -38,13 +47,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
