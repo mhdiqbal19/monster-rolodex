07eba32048e579328677f66ff238d45acea4c2c9
MUHAMMAD IQBAL <43297852+mhdiqbal19@users.noreply.github.com>
2022-12-28 00:08:09 +0700

    create card

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,29 +1,14 @@
 import { Component } from "react";
+import Card from "../card/card.component";
 import "./card-list.style.css";
 
 class CardList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
   render() {
     const { monsters } = this.props;
     return (
       <div className="card-list">
         {monsters.map((monster) => {
-          const { name, email, id } = monster;
-          return (
-            <div className="card-container" key={id}>
-              <img
-                alt={`monster ${name}`}
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card monster={monster} />;
         })}
       </div>
     );
